152923e2065c34e912c05bdf13469ae98c15c5b9
pyu666 <32445749+pyu666@users.noreply.github.com>
2018-10-25 19:22:49 +0900

    動かなかったのを修正

--- send_IFTTT.py
+++ send_IFTTT.py
@@ -5,12 +5,12 @@ import datetime
 def send(out_data, campus):
     re_data = ""
     for i in range(0, len(out_data)):
-    re_data = re_data + list[i] + "\n"
-    if (i % 4 == 0and i != 0):
-        print(re_data)
-        re_data = ""
-    elif(len(out_data) == i + 1):
-        print(re_data)
+        re_data = re_data +"\n"+ out_data[i]
+        if i % 4 == 0 and i != 0:
+            ifttt(re_data,campus)
+            re_data = ""
+        elif len(out_data) == i + 1:
+            ifttt(re_data,campus)
 # def send(out_data, campus):
 #     # today = day.strftime("%-m/%-d")
 #     re_data = split_str(out_data)
@@ -29,10 +29,9 @@ def ifttt(out_data, campus):
     today = "{}/{}".format(day.month, day.day)
     send_data = {"value1": today, "value2": out_data, "value3": campus}
     headers = {'Content-Type': "application/json"}
+
     url = 'YOUR_IFTTT_URL'
     response = requests.post(url, json=send_data, headers=headers)
     print(response.status_code)
-
-
 if __name__ == '__main__':
-    send(["test_data"], "test_campus")
+    send(["1","2","3","4","5","6"], "test_campus")
